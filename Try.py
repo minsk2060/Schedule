cc105b7e5ba053a43fca2cbcee15573fac6e4788
Mixail132 <millionbaxov@gmail.com>
2023-02-13 16:36:01 +0300

    Changing Try.py

--- Try.py
+++ Try.py
@@ -1,9 +1,32 @@
+import pywinauto
 from pywinauto.application import Application
 
-Application().start("C://Program Files (x86)/Microsoft/Edge/Application/msedge.exe")
-#.connect(path="C://Program Files (x86)/Microsoft/Edge/Application/msedge.exe")
+from pywinauto import Desktop
 
-#app.start("C://Program Files (x86)/Microsoft/Edge/Application/msedge.exe")
-# app = Application().connect(path="C:/Program Files/Internet Explorer/iexplore.exe")
-# app.window().set_focus()
+# app =Application(backend ="uia").connect(path="C://Program Files (x86)/Microsoft/Edge/Application/msedge.exe")
+# app.window(title_re='.*- Microsoft Edge', control_type="Window").set_focus()
+# d = Desktop(backend='uia')
+# main_window = d.window(title_re='.*- Microsoft Edge', control_type="Window")
+# main_window.dump_tree()
+# main_window.set_focus()
+# windows = Desktop(backend="uia").windows()
+#
+# for w in windows:
+#     print(w)
+#     print(type(w))
+#     #print(w.window_text())
 
+# app = Application().connect(path=r"C:/Windows/System32/notepad.exe")
+# app = Application(backend="uia").connect(path="C://Program Files (x86)/Microsoft/Edge/Application/msedge.exe")
+# # dlgspec = app.window()
+# pywinauto.findwindows.enum_windows()
+# print(pywinauto.findwindows.enum_windows())
+# app.window(title_re = "*pywinauto").set_focus()
+# app = Application(backend="uia").connect(path="C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe")
+# print(app.process)
+# print(app.windows())
+app = Application(backend="uia").connect(title_re=u".*Microsoft\u200b Edge", timeout=10)
+app.window().set_focus()
+# print(app.process)
+# print(app.windows())
+# app.window(title_re=u".*Microsoft\u200b Edge").dump_tree()
