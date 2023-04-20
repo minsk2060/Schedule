46f5162b158b89ccce3b74481a3b5969a8c4c94e
Mixail132 <millionbaxov@gmail.com>
2023-04-20 08:17:09 +0300

    Adding .gitignore

--- Try.py
+++ Try.py
@@ -1,8 +1,24 @@
-import requests
-import json
+import webbrowser, schedule, time
+from selenium import webdriver
+from pywinauto.application import Application
+
+# PREFX = "http://www.google.com"
+# app = Application(backend="uia")
+# app.connect(title_re=".*Microsoft\u200b Edge", timeout=10)
+# app.window().set_focus()
+# webbrowser.open_new_tab(PREFX)
+# driver = webdriver.Edge(executable_path ="C://Program Files (x86)/Microsoft/Edge/Application/edgedriver_win64/msedgedriver.exe")
+# driver = webdriver.Edge()
+driver = webdriver.Edge()
+driver.get("https://www.google.com/")
+driver.get("https://www.google.com/")
+driver.close()
 
 
-response = requests.post(url = "https://indel.becloud.by/", headers ={"Content-Type" : "application/json"} )
-print(response.headers)
-print(response.status_code)
-print(response.json())
+
+
+
+
+
+
+
