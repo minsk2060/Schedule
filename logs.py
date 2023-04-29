cfd9aa9d16011db517782d501bc378f6c6ec0444
minsk2060 <minsk2060@gmail.com>
2023-04-29 15:12:16 +0300

    Use replace to the items of the schedule -no results

--- logs.py
+++ logs.py
@@ -28,17 +28,17 @@ def acting (plant_log, par):
     action = ""
     a = par[-1:]
     # If other plants
-    if a == "1":   action = "  Пуск на низкой скорости"
+    if   a == "1": action = "  Пуск на низкой скорости"
     elif a == "2": action = "  Пуск на высокой скорости"
     elif a == "0": action = "  Cтоп"
     # If swimming pool
     if plant_log == "8388883&did=33560432":
-        if a == "0":   action = "  Выключение подсветки"
+        if   a == "0": action = "  Выключение подсветки"
         elif a == "2": action = "  Включение желтой подсветки"
         elif a == "1": action = "  Включение синей подсветки"
     # If dryers
     elif plant_log == "79691782&did=33556432" or plant_log == "79691777&did=33555432":
-        if a == "5":   action = "  Стоп"
+        if   a == "5": action = "  Стоп"
         elif a == "1": action = "  Пуск в режиме хоккей"
         elif a == "2": action = "  Пуск в режиме фигурное катание"
     return action
