0cc8e010d45e17170de9ab666a04def1a8136198
minsk2060 <minsk2060@gmail.com>
2023-05-06 08:48:01 +0300

    123

--- Schedule.py
+++ Schedule.py
@@ -35,7 +35,7 @@ def runschedule():                                                              
     # Execute all the schedules
     for i in range(len(cleartasks)):
         exec(f"""schedule.every().{cleartasks[i][1]}.at('{cleartasks[i][2]}').do(turn,'{cleartasks[i][0]}','&vid=17&value={cleartasks[i][3]}')""")
-    schedule.every(10).minutes.do(runschedule)                                    # Repeat reading The schedule
+    schedule.every(9).minutes.do(runschedule)                                    # Repeat reading The schedule
 
     cleartasks.clear()                                                            # Clear the list of no empty lines
     tasks.clear()
