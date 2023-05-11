e55bd0693407113a4b4c43de7664eec6b3e72056
minsk2060 <minsk2060@gmail.com>
2023-05-11 17:36:03 +0300

    Added a new function browsing(), added some type hints for the function

--- Schedule.py
+++ Schedule.py
@@ -7,46 +7,57 @@ from excels import readschedule
 
 print("РАБОТАЕТ УПРАВЛЕНИЕ ОБОРУДОВАНИЕМ ПО РАСПИСАНИЮ, НЕ ЗАКРЫВАЙТЕ ЭТО ОКНО")
 
-tasks = []                                                                        # The list holds all the single[] lists  even the blank ones
+tasks = []
 
-# Sending the request to the browser string
+def browsing():
+    """
+    browsing()  - установка фокуса на окне браузера Edge
+    """
+    app =Application(backend ="uia")
+    app.connect(title_re=u".*Microsoft\u200b Edge", timeout =10)
+    app.window().set_focus()
+
+
 def turn(get_plant, par):
     """
-    turn()       - действие (включить, выключить и т.п.)
+    turn()       - выполнение действия (включить, выключить и т.п.)
     get_plant    - код установки для вставки в строку запроса
     par          - параметр ( 0 -стоп, 1 пуск и т.п.)
     logging()    - запись в лог файл
-    PREFX        - общий набор в строке запроса
     app          - объект для подключения и установки фокуса на окне браузера Edge
     webbrowser   - отправка запроса в браузер
     time.sleep() - задержка времени
     hotkey()     - закрытие вкладки браузера с запросом
     """
     logging(get_plant, par)
-    PREFX = "http://192.168.250.50/ajaxjson/bac/setValue?pid=85&oid="
-    app = Application(backend="uia")
-    app.connect(title_re=u".*Microsoft\u200b Edge", timeout=10)
-    app.window().set_focus()
-    webbrowser.open_new_tab(PREFX + get_plant + par)
+    browsing()
+    webbrowser.open_new_tab("http://192.168.250.50/ajaxjson/bac/setValue?pid=85&oid=" + get_plant + par)
     time.sleep(3)
     hotkey("ctrl", "w")
 
-def runschedule():                                                                # Call the function to start the process
-    cleartasks = readschedule(tasks).copy()                                       # Call the function to read excel. Make a copy of the result list
-    for t in range(len(tasks)):                                                   # Run through the list of all of the lists
-        if tasks[t][2] == "None":                                                 # If there is no data in it
-            cleartasks.remove(tasks[t])                                           # Delete a nested empty list
-    schedule.clear()                                                              # Clear the previous schedule, cause it might been changed
-    # Execute all the schedules
+
+def runschedule():
+    """
+    runschedule()  - получение расписания, улаоение пустых значений,
+                     компоновка задач и запуск действий по расписанию.
+    readschedule() - чтение расписания из excel файла
+                     принимает пустой список tasks, возвращает заполненный список
+    cleartasks     - список, где не будет пустых значений
+
+    """
+    cleartasks = readschedule(tasks).copy()
+        for t in range(len(tasks)):
+        if tasks[t][2] == "None":
+            cleartasks.remove(tasks[t])
+    schedule.clear()
     for i in range(len(cleartasks)):
         exec(f"""schedule.every().{cleartasks[i][1]}.at('{cleartasks[i][2]}').do(turn,'{cleartasks[i][0]}','&vid=17&value={cleartasks[i][3]}')""")
-    schedule.every(10).minutes.do(runschedule)                                    # Repeat reading The schedule
-    cleartasks.clear()                                                            # Clear the list of no empty lines
+    schedule.every(10).minutes.do(runschedule)
+    cleartasks.clear()
     tasks.clear()
 
-runschedule()
 
-# schedule.every(3).seconds.do(runschedule)                                         # Repeat reading The schedule
+runschedule()
 
 
 while True:
