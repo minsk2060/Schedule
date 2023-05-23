21388316c8b7246601a59e27f76aaa78e4514be8
minsk2060 <minsk2060@gmail.com>
2023-05-23 07:04:02 +0300

    Старт выполнения логгирования аварий

--- request.py
+++ request.py
@@ -21,16 +21,15 @@ def switch(get_plant,par):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.42",
         "X-Requested-With": "XMLHttpRequest"
         }
-
     cookies = {
         "JSESSIONID":     "13reb3vcok495lblc0opr7kkg",
         "pw":             "73F0CF35AF3850221A1D641803815D686EC21A9316F6CFBFF079AE3B9BFE8DEA",
         "BAYEUX_BROWSER": "ec3egd91iy8tb3mjlhulodl01any",
         "uid":            "0"
-                }
+        }
 
     r = requests.get(url, headers=headers, cookies=cookies)
-
+    print(r.json())
 
 """
 Данные ниже получены из сети
@@ -58,4 +57,38 @@ User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,
 X-Requested-With: XMLHttpRequest"""
 
 if __name__ == "__main__":
-    switch("8388815&did=33561432", "&vid=17&value=1")
+    # switch('8388858&did=33557432', "&vid=17&value=1")
+    headers = {
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "en-GB,en;q=0.9,en-US;q=0.8",
+        "Connection": "keep-alive",
+        "Host": "192.168.250.50",
+        "Referer": "http://192.168.250.50/svo/graphic?oid=121634830&did=-1&vid=80",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.42",
+        "X-Requested-With": "XMLHttpRequest"
+    }
+    cookies = {
+        "JSESSIONID": "13reb3vcok495lblc0opr7kkg",
+        "pw": "73F0CF35AF3850221A1D641803815D686EC21A9316F6CFBFF079AE3B9BFE8DEA",
+        "BAYEUX_BROWSER": "ec3egd91iy8tb3mjlhulodl01any",
+        "uid": "0"
+    }
+    #url="http://192.168.250.50/svo/details/?oid=12584036&did=33557432&vid=17"
+    url="http://192.168.250.50/bac/details/update?oid=12584036&did=33557432&vid=17"
+    r = requests.get(url, headers=headers, cookies=cookies)
+    print(r.json()["data"]["79"][0])
+    print(r.json()["data"]["79"][0])
+    # alarm ="Alarm: true"
+    # if alarm in r.text():
+    #     print(alarm)
+"""
+{'data': {   '79': ['Binary Input', ''], 
+             '114': ['*', ''],
+             '77': ['PV1.7_DIA184', ''],
+             '115': ['*', ''],
+             '113': ['0', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=113&oid=12584036&did=33557432&vid=17">Edit</a>'],
+             '111': ['<span class="icon-status icon-statusflag-0-grey " title="Alarm: true"></span> <span class="icon-status icon-statusflag-1-grey  ui-state-disabled" title="Fault: false"></span> <span class="icon-status icon-statusflag-2-grey  ui-state-disabled" title="Overridden: false"></span> <span class="icon-status icon-statusflag-3-grey  ui-state-disabled" title="Out of service: false"></span> ', ''],
+             '35': ['<span class="icon-status icon-transition-0-grey " title="To Off-normal: true"></span> <span class="icon-status icon-transition-1-grey  ui-state-disabled" title="To Fault: false"></span> <span class="icon-status icon-transition-2-grey  ui-state-disabled" title="To Normal: false"></span> ', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=35&oid=12584036&did=33557432&vid=17">Edit</a>'], '17': ['2', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=17&oid=12584036&did=33557432&vid=17">Edit</a>'], '36': ['Off-normal', ''], '33': ['3680502', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=33&oid=12584036&did=33557432&vid=17">Edit</a>'], '15': ['293', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=15&oid=12584036&did=33557432&vid=17">Edit</a>'], '16': ['22.05.2023 16:29:46', ''], '8263': ['-', ''], '103': ['No fault detected', '<a class ="editbutton  disabled " href="/ajaxjson/bac/getEditOptions?pid=103&oid=12584036&did=33557432&vid=17">Edit</a>'], '81': ['false', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=81&oid=12584036&did=33557432&vid=17">Edit</a>'], '130': ['<div style="white-space:nowrap;margin-top:4px;"><span class="icon-status icon-transition-0-grey " title="To Off-normal"></span>  <span style="white-space:nowrap;">22.05.2023 16:29:46</span></div><div style="white-space:nowrap;margin-top:4px;"><span class="icon-status icon-transition-1-grey " title="To Fault"></span>  <span style="white-space:nowrap;">-</span></div><div style="white-space:nowrap;margin-top:4px;"><span class="icon-status icon-transition-2-grey " title="To Normal"></span>  <span style="white-space:nowrap;">-</span></div>', ''], '84': ['0', ''], '8264': ['-', ''], '85': ['ДА <span class="text-right "></span>', '<a class ="editbutton  disabled " href="/ajaxjson/bac/getEditOptions?pid=85&oid=12584036&did=33557432&vid=17">Edit</a>'], '46': ['НЕТ', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=46&oid=12584036&did=33557432&vid=17">Edit</a>'], '371': ['-', ''], '28': ['Общая авария: A', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=28&oid=12584036&did=33557432&vid=17">Edit</a>'], '168': ['17-XL_WEB_BI', ''], '351': ['-', ''], '353': ['-', ''], '352': ['-', ''], '355': ['-', ''], '0': ['<span class="icon-status icon-transition-0-grey  ui-state-disabled" title="To Off-normal: false"></span> <span class="icon-status icon-transition-1-grey " title="To Fault: true"></span> <span class="icon-status icon-transition-2-grey " title="To Normal: true"></span> ', ''], '354': ['-', ''], '6': ['ДА', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=6&oid=12584036&did=33557432&vid=17">Edit</a>'], '356': ['-', '<a class ="editbutton  disabled " href="/ajaxjson/bac/getEditOptions?pid=356&oid=12584036&did=33557432&vid=17">Edit</a>'], '31': ['-', ''], '4': ['ДА', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=4&oid=12584036&did=33557432&vid=17">Edit</a>'], '72': ['Alarm', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=72&oid=12584036&did=33557432&vid=17">Edit</a>'], '75': ['Binary Input 1124', '']}, 'fields': ['property-value', 'property-edit']}
+
+"""
