e369a5abc923dc7f347b0c59aaecb9200000496e
minsk2060 <minsk2060@gmail.com>
2023-05-26 18:51:47 +0300

    123

--- request.py
+++ request.py
@@ -49,13 +49,17 @@ if __name__ == "__main__":
         "uid": "0"
     }
     #url="http://192.168.250.50/svo/details/?oid=12584036&did=33557432&vid=17"
-    url="http://192.168.250.50/bac/details/update?oid=12584036&did=33557432&vid=17"
-    r = requests.get(url, headers=headers, cookies=cookies)
-    print(r.json()["data"]["79"][0])
-    print(r.json()["data"]["79"][0])
-    # alarm ="Alarm: true"
-    # if alarm in r.text():
-    #     print(alarm)
+    url ="http://192.168.250.50/bac/details/update?oid=12584036&did=33557432&vid=17" # ПВ-1.7
+    url2="http://192.168.250.50/bac/details/update?oid=12584092&did=33557432&vid=17" # ПВ-1.6
+    alarm = "Alarm: true"
+    r = requests.get(url2, headers=headers, cookies=cookies)
+    # print(r.json()["data"]["111"][0])
+    # print(r.json()["data"]["111"][0])
+    if alarm in r.json()["data"]["111"][0]:
+      print(alarm)
+    else:
+        print("Alarm: False")
+
 """
 {'data': {   '79': ['Binary Input', ''], 
              '114': ['*', ''],
@@ -63,6 +67,29 @@ if __name__ == "__main__":
              '115': ['*', ''],
              '113': ['0', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=113&oid=12584036&did=33557432&vid=17">Edit</a>'],
              '111': ['<span class="icon-status icon-statusflag-0-grey " title="Alarm: true"></span> <span class="icon-status icon-statusflag-1-grey  ui-state-disabled" title="Fault: false"></span> <span class="icon-status icon-statusflag-2-grey  ui-state-disabled" title="Overridden: false"></span> <span class="icon-status icon-statusflag-3-grey  ui-state-disabled" title="Out of service: false"></span> ', ''],
-             '35': ['<span class="icon-status icon-transition-0-grey " title="To Off-normal: true"></span> <span class="icon-status icon-transition-1-grey  ui-state-disabled" title="To Fault: false"></span> <span class="icon-status icon-transition-2-grey  ui-state-disabled" title="To Normal: false"></span> ', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=35&oid=12584036&did=33557432&vid=17">Edit</a>'], '17': ['2', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=17&oid=12584036&did=33557432&vid=17">Edit</a>'], '36': ['Off-normal', ''], '33': ['3680502', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=33&oid=12584036&did=33557432&vid=17">Edit</a>'], '15': ['293', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=15&oid=12584036&did=33557432&vid=17">Edit</a>'], '16': ['22.05.2023 16:29:46', ''], '8263': ['-', ''], '103': ['No fault detected', '<a class ="editbutton  disabled " href="/ajaxjson/bac/getEditOptions?pid=103&oid=12584036&did=33557432&vid=17">Edit</a>'], '81': ['false', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=81&oid=12584036&did=33557432&vid=17">Edit</a>'], '130': ['<div style="white-space:nowrap;margin-top:4px;"><span class="icon-status icon-transition-0-grey " title="To Off-normal"></span>  <span style="white-space:nowrap;">22.05.2023 16:29:46</span></div><div style="white-space:nowrap;margin-top:4px;"><span class="icon-status icon-transition-1-grey " title="To Fault"></span>  <span style="white-space:nowrap;">-</span></div><div style="white-space:nowrap;margin-top:4px;"><span class="icon-status icon-transition-2-grey " title="To Normal"></span>  <span style="white-space:nowrap;">-</span></div>', ''], '84': ['0', ''], '8264': ['-', ''], '85': ['ДА <span class="text-right "></span>', '<a class ="editbutton  disabled " href="/ajaxjson/bac/getEditOptions?pid=85&oid=12584036&did=33557432&vid=17">Edit</a>'], '46': ['НЕТ', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=46&oid=12584036&did=33557432&vid=17">Edit</a>'], '371': ['-', ''], '28': ['Общая авария: A', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=28&oid=12584036&did=33557432&vid=17">Edit</a>'], '168': ['17-XL_WEB_BI', ''], '351': ['-', ''], '353': ['-', ''], '352': ['-', ''], '355': ['-', ''], '0': ['<span class="icon-status icon-transition-0-grey  ui-state-disabled" title="To Off-normal: false"></span> <span class="icon-status icon-transition-1-grey " title="To Fault: true"></span> <span class="icon-status icon-transition-2-grey " title="To Normal: true"></span> ', ''], '354': ['-', ''], '6': ['ДА', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=6&oid=12584036&did=33557432&vid=17">Edit</a>'], '356': ['-', '<a class ="editbutton  disabled " href="/ajaxjson/bac/getEditOptions?pid=356&oid=12584036&did=33557432&vid=17">Edit</a>'], '31': ['-', ''], '4': ['ДА', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=4&oid=12584036&did=33557432&vid=17">Edit</a>'], '72': ['Alarm', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=72&oid=12584036&did=33557432&vid=17">Edit</a>'], '75': ['Binary Input 1124', '']}, 'fields': ['property-value', 'property-edit']}
+             '35': ['<span class="icon-status icon-transition-0-grey " title="To Off-normal: true"></span> <span class="icon-status icon-transition-1-grey  ui-state-disabled" title="To Fault: false"></span> <span class="icon-status icon-transition-2-grey  ui-state-disabled" title="To Normal: false"></span> ', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=35&oid=12584036&did=33557432&vid=17">Edit</a>'], 
+             '17': ['2', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=17&oid=12584036&did=33557432&vid=17">Edit</a>'],
+             '36': ['Off-normal', ''], 
+             '33': ['3680502', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=33&oid=12584036&did=33557432&vid=17">Edit</a>'], 
+             '15': ['293', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=15&oid=12584036&did=33557432&vid=17">Edit</a>'], 
+             '16': ['22.05.2023 16:29:46', ''], 
+             '8263': ['-', ''], 
+             '103': ['No fault detected', '<a class ="editbutton  disabled " href="/ajaxjson/bac/getEditOptions?pid=103&oid=12584036&did=33557432&vid=17">Edit</a>'], 
+             '81': ['false', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=81&oid=12584036&did=33557432&vid=17">Edit</a>'], 
+             '130': ['<div style="white-space:nowrap;margin-top:4px;"><span class="icon-status icon-transition-0-grey " title="To Off-normal"></span>  <span style="white-space:nowrap;">22.05.2023 16:29:46</span></div><div style="white-space:nowrap;margin-top:4px;"><span class="icon-status icon-transition-1-grey " title="To Fault"></span>  <span style="white-space:nowrap;">-</span></div><div style="white-space:nowrap;margin-top:4px;"><span class="icon-status icon-transition-2-grey " title="To Normal"></span>  <span style="white-space:nowrap;">-</span></div>', ''], 
+             '84': ['0', ''], '8264': ['-', ''], '85': ['ДА <span class="text-right "></span>', '<a class ="editbutton  disabled " href="/ajaxjson/bac/getEditOptions?pid=85&oid=12584036&did=33557432&vid=17">Edit</a>'], 
+             '46': ['НЕТ', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=46&oid=12584036&did=33557432&vid=17">Edit</a>'], 
+             '371': ['-', ''], '28': ['Общая авария: A', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=28&oid=12584036&did=33557432&vid=17">Edit</a>'], 
+             '168': ['17-XL_WEB_BI', ''], 
+             '351': ['-', ''], 
+             '353': ['-', ''], 
+             '352': ['-', ''], 
+             '355': ['-', ''], 
+             '0': ['<span class="icon-status icon-transition-0-grey  ui-state-disabled" title="To Off-normal: false"></span> <span class="icon-status icon-transition-1-grey " title="To Fault: true"></span> <span class="icon-status icon-transition-2-grey " title="To Normal: true"></span> ', ''], 
+             '354': ['-', ''], '6': ['ДА', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=6&oid=12584036&did=33557432&vid=17">Edit</a>'], 
+             '356': ['-', '<a class ="editbutton  disabled " href="/ajaxjson/bac/getEditOptions?pid=356&oid=12584036&did=33557432&vid=17">Edit</a>'], 
+             '31': ['-', ''], '4': ['ДА', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=4&oid=12584036&did=33557432&vid=17">Edit</a>'], 
+             '72': ['Alarm', '<a class ="editbutton " href="/ajaxjson/bac/getEditOptions?pid=72&oid=12584036&did=33557432&vid=17">Edit</a>'], 
+             '75': ['Binary Input 1124', '']}, 'fields': ['property-value', 'property-edit']}
 
 """
