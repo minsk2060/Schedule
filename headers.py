d01fe51f14deb4e34be51be1a575d21424677e17
Mixail132 <millionbaxov@gmail.com>
2023-06-03 08:34:16 +0300

    Изменен заголовок для запросов об аварии класса А

--- headers.py
+++ headers.py
@@ -1,4 +1,3 @@
-
 header ={
         "Accept": "application/json, text/javascript, */*; q=0.01",
         "Accept-Encoding": "gzip, deflate",
@@ -9,9 +8,45 @@ header ={
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.42",
         "X-Requested-With": "XMLHttpRequest"
     }
+
 cookie = {
     "JSESSIONID": "13reb3vcok495lblc0opr7kkg",
     "pw": "73F0CF35AF3850221A1D641803815D686EC21A9316F6CFBFF079AE3B9BFE8DEA",
     "BAYEUX_BROWSER": "ec3egd91iy8tb3mjlhulodl01any",
     "uid": "0"
 }
+#об-2.8
+header_alarm_A={
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "en-GB,en;q=0.9,en-US;q=0.8",
+    "Connection": "keep-alive",
+    "Host": "192.168.250.50","
+    # "Referer": "http://192.168.250.50/svo/simple?oid=121634826&did=-1&vid=80&user=true",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.57",
+"""
+"""
+об-2.7
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7
+Accept-Encoding: gzip, deflate
+Accept-Language: en-GB,en;q=0.9,en-US;q=0.8
+Connection: keep-alive
+Cookie: JSESSIONID=1cz6kij7qyzyvkjkekql6inte; uid=0; pw=73F0CF35AF3850221A1D641803815D686EC21A9316F6CFBFF079AE3B9BFE8DEA; BAYEUX_BROWSER=f924-1swhe0fqnhm5alieq2f7r1d7o
+Host: 192.168.250.50
+Referer: http://192.168.250.50/svo/simple?oid=121634826&did=-1&vid=80
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.57
+"""
+"""
+об-2.15
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7
+Accept-Encoding: gzip, deflate
+Accept-Language: en-GB,en;q=0.9,en-US;q=0.8
+Connection: keep-alive
+Cookie: JSESSIONID=1cz6kij7qyzyvkjkekql6inte; uid=0; pw=73F0CF35AF3850221A1D641803815D686EC21A9316F6CFBFF079AE3B9BFE8DEA; BAYEUX_BROWSER=f924-1swhe0fqnhm5alieq2f7r1d7o
+Host: 192.168.250.50
+Referer: http://192.168.250.50/svo/simple?oid=121634825&did=-1&vid=80
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.57
+"""
--- request.py
+++ request.py
@@ -19,7 +19,7 @@ def switch(get_plant,par):
 def getalarms():
         for i,j in alarms_try.items():
             url=f"http://192.168.250.50/svo/details/update?oid={i}&vid=17"
-            r=requests.get(url, headers=header, cookies=cookie, allow_redirects=False)
+            r=requests.get(url, headers=header_alarm_A, cookies=cookie, allow_redirects=False)
             for s in range(10):
                 if r.status_code == 200:
                     print (r.status_code)
@@ -42,7 +42,7 @@ def getalarms():
 if __name__ == "__main__":
     # switch('8388858&did=33557432', "&vid=17&value=1")
     # url="http://192.168.250.50/svo/details/?oid=12584036&did=33557432&vid=17"
-    url ="http://192.168.250.50/bac/details/update?oid=12584036&did=33557432&vid=17" # ПВ-1.7
+    # url ="http://192.168.250.50/bac/details/update?oid=12584036&did=33557432&vid=17" # ПВ-1.7
     # url2="http://192.168.250.50/bac/details/update?oid=12584092&did=33557432&vid=17" # ПВ-1.6
     # alarm = "Alarm: true"
     #r = requests.get(url, headers=header, cookies=cookie)
