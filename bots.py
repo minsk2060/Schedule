9c11a0b5f401b3412dd79611fd8f31b93ddb7e03
minsk2060 <minsk2060@gmail.com>
2023-06-15 18:01:30 +0300

    Подготовка фуекции для отправки сообщений только тем рабочим, чья смена сегодня

--- bots.py
+++ bots.py
@@ -1,10 +1,7 @@
 import telebot
-from tokens import telegramtoken, chat_id
+from tokens import telegramtoken, chat_id_bosses, chat_id_workers
+import datetime
 
-# chat_id = {"Mikhail" : "1140621075",
-#            "Michael" : "5740110040",
-#            "Roman"   : "1565146153",
-#            }
 
 def telegram(msg):
     """
@@ -15,8 +12,40 @@ def telegram(msg):
     chat_id        - идентификаторы получателей сообщений в Telegram
     """
     bot = telebot.TeleBot(telegramtoken)
-    for i in chat_id.values():
+    for i in chat_id_bosses.values():
         bot.send_message(i, msg)
+    # worker = onduties()  # выполняя функцию, получаем ключ (имя получателя) для словаря с именами и id
+    # bot.send_message(chat_id_workers[worker], msg)  # отправляем сообщение тому, кто сейчас на смене
+
+def onduties():
+    """Функция предусматривает определение кто сегодня на смене в хладоцентре.
+    Восстановить эту функцию когда окончится сезон отпусков и график восстановится"""
+    dat = datetime.datetime().now() # определить сегодняшнюю дату
+    d = dat.toordinal() % 4         # найти порядковый номер даты и найти остаток от деления на 4
+    if datetime.time(8, 00) < dat.time() < datetime.time(23, 59): # определить период первой части смены (8:00...0:00)
+        # Фамилия, результат деления для первой части смены и второй
+        # Вульфов  1, 2
+        # Клышко   2, 3
+        # Басыров  3, 0
+        # Федорчук 0, 1
+        if d == 1:
+            onduty = "Alexandr"
+        elif d == 2:
+            onduty = "Nikita"
+        elif d == 3:
+            onduty = "Yura"
+        elif d == 0:
+            onduty = "Zhenia"
+    elif datetime.time(0, 00) < dat.time() < datetime.time(8, 00): # определить период второй части смены 0:00...8:00
+        if d == 2:
+            onduty = "Alexandr"
+        elif d == 3:
+            onduty = "Nikita"
+        elif d == 0:
+            onduty = "Yura"
+        elif d == 1:
+            onduty = "Zhenia"
+    return onduty
 
 
 
--- trying/Try_4.py
+++ trying/Try_4.py
@@ -1,48 +1,35 @@
 import datetime
 
+# Фамилия, результат деления для первой части смены и второй
+# Вульфов  1, 2
+# Клышко   2, 3
+# Басыров  3, 0
+# Федорчук 0, 1
 
-#only_date = (datetime.today().toordinal())   # Здесь найдена только дата, когда он работает, нужно найти период
-# # print(only_date)
-delta_before = datetime.timedelta(days=0, hours=100)
-delta_after  = datetime.timedelta(days=0, hours=8)
-Voolfov_1 = datetime.datetime.fromordinal(datetime.datetime.today().toordinal()) - delta_before
-Voolfov_2 = datetime.datetime.fromordinal(datetime.datetime.today().toordinal()) + delta_after
+b = datetime.datetime(2023, 5, 31, 7, 10)
+d = b.toordinal() % 4
+if datetime.time(8, 00) < b.time() < datetime.time(23, 59):
+    half = "first"
+elif datetime.time(0, 00) < b.time() < datetime.time(8, 00):
+    half = "second"
+def onduties():
+    if datetime.time(8, 00) < b.time() < datetime.time(23, 59):
+        if d == 1:
+            onduty = "Alexandr"
+        elif d ==  2:
+            onduty = "Nikita"
+        elif d == 3:
+            onduty = "Yura"
+        elif d == 0:
+            onduty = "Zhenia"
+    elif datetime.time(0, 00) < b.time() < datetime.time(8, 00):
+        if d == 2:
+            onduty = "Alexandr"
+        elif d == 3:
+            onduty = "Nikita"
+        elif d == 0:
+            onduty = "Yura"
+        elif d == 1:
+            onduty = "Zhenia"
+    return onduty
 
-
-#print(datetime.datetime.now().toordinal()%4)
-#print((datetime.datetime.now()+delta_before).toordinal()%4)
-
-# Фамилия, резульат деления времени первой части смены и второй
-# Вульфов  3, 0
-# Клышко   0, 1
-# Басыров  1, 2
-# Федорчук 2, 3
-# Сначала определяем первая полусмена или вторая в зависимости от времени суток
-# Затем находим коэффициент
-# Затем определяем принадлежность коэффициента пользователю
-
-
-dat = datetime.datetime(2023,6,22,2,30)
-#print(datetime.time(8,00))
-# if datetime.time(8,00)< datetime.datetime.now().time() > datetime.time(0,00):
-#     print("Yes")
-# else:
-#     print("no")
-# a=738683
-# t=datetime.datetime.now().time()
-# print(t)
-#print(datetime.datetime.fromordinal(only_date))
-# Период это начало даты минус 16 часов и начало даты плюс 8 часов
-a=3
-b=4
-c=6
-if a<b<c:
-    print("True")
-
-# """
-# 12.06.2023 - 738683
-# Вульфов  -3
-# Клышко
-# Басыров
-# Федорчук
-# """
