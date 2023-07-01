86df7550b262146e01215034f362085a140e407f
Mixail132 <millionbaxov@gmail.com>
2023-07-01 20:37:37 +0300

    Небольшое пробное изменение в excels.py с целью упростиь код

--- excels.py
+++ excels.py
@@ -31,17 +31,18 @@ def readschedule(tasks):
     """
     workbook = load_workbook(schedule_book)
     for j in range(53, 383, 10):
-        for k in range(7):
+        for k in range(1, 6, 1):
             single.append(str(workbook.active.cell(row=j, column=4).value))
             for s in [2, 5, 3]:
-                single.append(str(workbook.active.cell(row=j + 1 + k, column=s).value))
+                single.append(str(workbook.active.cell(row=j +  k, column=s).value))
             #single.append(str(workbook.active.cell(row=j + 1 + k, column=5).value))
             #single.append(str(workbook.active.cell(row=j + 1 + k, column=3).value))
+            #это работает , но можно сократить еще, итерируясь по списку , а не range(7)
             refresh(tasks)
             #for s in [4, 2, 6]:
-            single.append(str(workbook.active.cell(row=j, column=4).value))
-            single.append(str(workbook.active.cell(row=j + 1 + k, column=2).value))
-            single.append(str(workbook.active.cell(row=j + 1 + k, column=6).value))
+            single.append(str(workbook.active.cell(row=j +  0, column=4).value))
+            single.append(str(workbook.active.cell(row=j +  k, column=2).value))
+            single.append(str(workbook.active.cell(row=j +  k, column=6).value))
             driers = str(workbook.active.cell(row=j, column=4).value)
             if driers == "79691782&did=33556432" or driers == "79691777&did=33555432":
                 single.append("5")
@@ -50,15 +51,15 @@ def readschedule(tasks):
             #driers = ""
             refresh(tasks)
             #for s in [4, 2, 8, 10]:
-            single.append(str(workbook.active.cell(row=j, column=4).value))
-            single.append(str(workbook.active.cell(row=j + 1 + k, column=2).value))
-            single.append(str(workbook.active.cell(row=j + 1 + k, column=8).value))
-            single.append(str(workbook.active.cell(row=j + 1 + k, column=10).value))
+            single.append(str(workbook.active.cell(row=j,      column=4).value))
+            single.append(str(workbook.active.cell(row=j +  k, column=2).value))
+            single.append(str(workbook.active.cell(row=j +  k, column=8).value))
+            single.append(str(workbook.active.cell(row=j +  k, column=10).value))
             refresh(tasks)
             #for s in [4, 2, 9]:
-            single.append(str(workbook.active.cell(row=j, column=4).value))
-            single.append(str(workbook.active.cell(row=j + 1 + k, column=2).value))
-            single.append(str(workbook.active.cell(row=j + 1 + k, column=9).value))
+            single.append(str(workbook.active.cell(row=j,      column=4).value))
+            single.append(str(workbook.active.cell(row=j +  k, column=2).value))
+            single.append(str(workbook.active.cell(row=j +  k, column=9).value))
             driers = str(workbook.active.cell(row=j, column=4).value)
             if driers == "79691782&did=33556432" or driers == "79691777&did=33555432":
                 single.append("5")
