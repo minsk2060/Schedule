3deb0edc82d7e56f5fd9ad6d8b0246700ef28d6c
Mixail132 <millionbaxov@gmail.com>
2023-07-02 10:10:41 +0300

    123

--- Schedule.py
+++ Schedule.py
@@ -43,7 +43,7 @@ def runschedule():
     cleartasks.clear()
     tasks.clear()
 
-schedule.every(30).seconds.do(runschedule)
+schedule.every(10).minutes.do(runschedule)
 schedule.every(13).minutes.do(getalarms, alarms_dict=alarms_A,  column_number=4, alarm_text='Авария класса А')
 schedule.every(23).minutes.do(getalarms, alarms_dict=alarms_BC, column_number=5, alarm_text='Авария класса B,C')
 
