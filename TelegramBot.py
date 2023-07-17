8b54d59d7975d954254e62fca2afa0de1c5cbec1
Mixail132 <millionbaxov@gmail.com>
2023-07-17 20:49:39 +0300

    123

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 from tokens import telegramtoken, telegram_users
 from duties import onduties
 
@@ -20,8 +21,18 @@ def to_telegram(msg):
     # отправляем сообщение тому, кто сейчас на смене
     # bot.send_message(chat_id_workers[worker], msg)
 
-if __name__ =="__main__":
-    bot = telebot.TeleBot(telegramtoken)
-    msg="Авария на объекте, просьба относиться с пониманием!"
-    for i in telegram_users.values():
-        bot.send_message(i, msg)
+# Первый шаг к созданию кнопки в боте
+# bot = telebot.TeleBot(telegramtoken)
+# @bot.message_handler(commands=["start"])
+# def start(message):
+#     markup = types.InlineKeyboardMarkup()
+#     button1 = types.InlineKeyboardButton("Пробная кнопка", url = "www.google.com")
+#     markup.add(button1)
+#     bot.send_message(message.chat.id, "Привет, {0.first_name}! Нажми на кнопку и перейди на сайт)".format(message.from_user), reply_markup=markup)
+# bot.polling(none_stop=True)
+
+# if __name__ =="__main__":
+#     bot = telebot.TeleBot(telegramtoken)
+#     msg="Авария на объекте, просьба относиться с пониманием!"
+#     for i in telegram_users.values():
+#         bot.send_message(i, msg)
