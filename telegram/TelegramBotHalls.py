0f9f3c5986821b7cc977f0f219d017c80c67b1b0
Mixail132 <millionbaxov@gmail.com>
2023-07-21 09:58:48 +0300

    123

--- telegram/TelegramBotHalls.py
+++ telegram/TelegramBotHalls.py
@@ -81,8 +81,9 @@ def func(message):
         # if "ПВ-2.7" in msg:
         #     plt = "ПВ-2.7"
         # else:
-        plt = msg.replace("Расписание  ", "")
-        bot.send_message(message.chat.id, text=f'{msg} на эти дни:\n\n{read_sched(plt)[-6:]}')
+        #plt = msg.replace("Расписание  ", "")
+        plt = msg[-6:]
+        bot.send_message(message.chat.id, text=f'{msg} на эти дни:\n\n{read_sched(plt)}')
         # fil = open("../logging/readlogs.txt", "r")
         # sts = []
         # als = fil.read()\
