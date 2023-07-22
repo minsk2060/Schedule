280ae5a7fba447a409d2e6e89951cf97e1003216
Mixail132 <millionbaxov@gmail.com>
2023-07-22 10:49:59 +0300

    "Изменен код для лог файла перезапуска телеграм бота"

--- telegram/runBot.py
+++ telegram/runBot.py
@@ -7,7 +7,7 @@ while True:
             for i in p.cmdline():
                 if "TelegramBotHalls.py" not in i:
                     import TelegramBotHalls
-                    now = datetime.datetime.now().strftime("%Y" "%h" "%m")
+                    now = datetime.datetime.now().strftime("%Y  %h  %m  %Н:%m")
                     print(f"Bot restarted in {now}")
                     f = open("logbot.txt", "a")
                     f.write(f"Телеграм бот вновь запущен {now}\n")
