f11ca31b515a54d9ffb0e74ab46eac15a4c2728d
minsk2060 <minsk2060@gmail.com>
2023-07-22 14:05:40 +0300

    Перезагрузка бота все равно не работает

--- telegram/runBot.py
+++ telegram/runBot.py
@@ -1,10 +1,11 @@
 import psutil, time, datetime
 
 while True:
-    time.sleep(10)
+    time.sleep(30)
     for p in psutil.process_iter():
         if "python" in p.name():
             for i in p.cmdline():
+                # print(i)
                 if "TelegramBotHalls.py" not in i:
                     import TelegramBotHalls
                     now = datetime.datetime.now()
