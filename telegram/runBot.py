c2c15985a50114ca29c7daadd5daa05c96035ca2
minsk2060 <minsk2060@gmail.com>
2023-07-22 14:49:22 +0300

    Не работает рестарт бота

--- telegram/runBot.py
+++ telegram/runBot.py
@@ -1,14 +1,25 @@
 import psutil, time, datetime
+import schedule
+import TelegramBotHalls
+# from TelegramBotHalls import bot
 
-while True:
-    time.sleep(30)
+def runbot():
     for p in psutil.process_iter():
         if "python" in p.name():
             for i in p.cmdline():
-                # print(i)
-                if "TelegramBotHalls.py" not in i:
-                    import TelegramBotHalls
-                    now = datetime.datetime.now()
+                if "TelegramBotHalls.py" in i:
+                    print("123")
+                    print(i)
+                    continue
+                else:
                     f = open("logbot.txt", "a")
-                    f.write(f"Телеграм бот вновь запущен {now}\n")
+                    f.write(f"Телеграм бот вновь запущен {datetime.datetime.now()}\n")
                     f.close()
+                    # bot.polling(none_stop=True, timeout=600, long_polling_timeout=600)
+                    import TelegramBotHalls
+
+schedule.every(1).minutes.do(runbot)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
